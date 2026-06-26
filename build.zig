d3dff3612785d2f76c9e9a3fb1e91b5c363aa3c2
bendy1234 <83143663+bendy1234@users.noreply.github.com>
2026-06-25 23:42:15 -0700

    Oops

--- build.zig
+++ build.zig
@@ -2,6 +2,6 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     _ = b.addModule("cairo", .{
-        .root_source_file = b.path("src/cairo/root.zig"),
+        .root_source_file = b.path("cairo.zig"),
     });
 }
